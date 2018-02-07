dce61c98ecd2617a077ecba182add6c2e581e4ef
Robert Laverty <roblav96@gmx.com>
2018-02-07 09:51:10 -0500

    npm doesnt like this repo name :O

--- src/index.ts
+++ src/index.ts
@@ -4,21 +4,37 @@ require('source-map-support').install()
 
 import * as eyes from 'eyes'
 import * as cron from 'cron'
-// import * as loudness from 'loudness'
+import * as loudness from 'loudness'
+import * as playsound from 'play-sound'
+
+
+
+const player = playsound({})
+
+function playMP3(path: string): Promise<void> {
+	return new Promise(function(resolve, reject) {
+		player.play(path, function(error) {
+			if (error) return
+		})
+	})
+}
+
+
+
+function onTick() {
+	playMP3('sounds/KzurbSonar.mp3').catch(function(error) {
+		console.error('player.play > error', error)
+	})
+}
 
 
 
 new cron.CronJob({
+	onTick,
 	cronTime: '*/15 * * * *',
 	start: true,
-	onTick: function() {
-		console.log('cron.CronJobzzz >')
-		eyes.inspect(cron.CronJob)
-	},
 	timeZone: 'America/New_York',
-	runOnInit: true,
+	runOnInit: process.argv.indexOf('--dev') >= 0,
 })
-
-
 
 
